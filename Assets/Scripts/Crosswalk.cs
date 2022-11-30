c48c1416aeeb027378bb297fc61a9d36036a2fb8
annaloeb <119467908+annaloeb@users.noreply.github.com>
2022-11-29 18:33:08 -0800

    Added Crosswalk and Pedestrian Classes

---
+++ Assets/Scripts/Crosswalk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosswalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
