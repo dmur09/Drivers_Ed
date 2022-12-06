29e8df779a54700e070502dc654e58cfc03e4c1f
annaloeb <119467908+annaloeb@users.noreply.github.com>
2022-12-05 17:35:41 -0800

    Added Sounds class and put sounds into sounds folder

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioClip HitGuardrailSound;
+    public static Sounds Instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this.gameObject);
+        Instance = this;
+    }
+
+    void Start()
+    {
+        audioSource = GameObject.FindGameObjectWithTag("AudioSource").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
