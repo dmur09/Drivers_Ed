a29aa563c17c5b1d89772b1940111cc5af90bfe2
annaloeb <119467908+annaloeb@users.noreply.github.com>
2022-12-08 14:42:42 -0800

    Updated speed/turning parameters

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public static class GameParameters
 {
-    public static float CarMoveAmount = .02f;
-    public static float MaxForwardSpeed = 11.0f;
+    public static float CarMoveAmount = .015f;
+    public static float MaxForwardSpeed = 7.0f;
     public static float MaxReverseSpeed = -11.0f;
-    public static float TurnSpeedScale = 15.0f;
+    public static float TurnSpeedScale = 9.0f;
 
 }
