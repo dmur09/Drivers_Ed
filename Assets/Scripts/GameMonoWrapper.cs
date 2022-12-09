3f94b165f339d3c66e42813d597e53a25480ae27
annaloeb <119467908+annaloeb@users.noreply.github.com>
2022-12-08 17:55:27 -0800

    Added minimap code to show/hide at start/end

--- Assets/Scripts/GameMonoWrapper.cs
+++ Assets/Scripts/GameMonoWrapper.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class GameMonoWrapper : MonoBehaviour
 {
     public UI UI;
+    public GameObject MiniMap;
     public GameTimer GameTimer;
     void Start()
     {
         Game.Initialize(UI, GameTimer);
+        MiniMap.SetActive(false);
     }
     
     void Update()
     {
-        if (Game.GameJustEnded())
+        if (Game.GameJustEnded() || DamageGauge.damage <= 0)
         {
             Game.EndGame();
+            MiniMap.SetActive(false);
         }
     }
 
     public void StartGame()
     {
         Game.StartGame();
+        MiniMap.SetActive(true);
     }
 }
